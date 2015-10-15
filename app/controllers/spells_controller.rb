9876f43251c2a83b9ba83c52086145732628155d
Dustin Perzanowski <perzanowski.4@osu.edu>
2015-10-15 14:00:52 -0400

    set up edit spell

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,15 +1,49 @@
 class SpellsController < ApplicationController
+  before_action :set_spell, only: %i[new create edit update show destroy]
+
+  respond_to :html, :js, :json
 
   def index
     @spells = Spell.all
+    respond_with @spells
+  end
+
+  def new
+    respond_with @spell
+  end
+
+  def create
+
+  end
+
+  # def edit
+  #
+  # end
+
+  def update
+    @spell.update(spell_params)
+
+    respond_with @spell
   end
 
   def show
-    @spell = Spell.find(params[:id])
+    respond_with @spell
+  end
+
+  def destroy
+
   end
 
   private
   def spell_params
     params.require(:spell).permit(policy(@spell || Spell).permitted_attributes)
+  end
+
+  def set_spell
+    @spell ||= case action_name
+      when *%w(new) then Spell.new
+      when *%w(create) then Spell.new(spell_params)
+      when *%w(show edit update destroy) then Spell.find(params[:id])
+    end
   end
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,4 +1,16 @@
 class Spell < ActiveRecord::Base
+  enum spell_levels: [
+    "Cantrip",
+    "Level 1",
+    "Level 2",
+    "Level 3",
+    "Level 4",
+    "Level 5",
+    "Level 6",
+    "Level 7",
+    "Level 8",
+    "Level 9", 
+  ]
   def print_level
     if self.level == 0
       return "Cantrip"
