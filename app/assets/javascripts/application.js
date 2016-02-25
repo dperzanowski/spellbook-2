475e49efaa05dc1f764035787323c0d17a3c01b1
Dustin Perzanowski <perzanowski.4@osu.edu>
2016-02-25 16:38:49 -0500

    ajaxify the spell filter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,37 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+(function($){
+  $.fn.ajaxFiltering = function() {
+    // cache spell-filter DOM element
+    var spellFilter = this;
+
+    // remove submit button
+    $('input.btn', spellFilter).remove();
+
+    // add reset button
+    spellFilter.append('<input id="reset" class="btn btn-default" type="reset" value="Reset">');
+    $('input#reset', spellFilter).click(function(event) {
+      $('input[type="checkbox"]', spellFilter).attr('checked', false);
+      spellFilter.submit();
+    });
+
+    // submit form when checkbox selected
+    $('input[type="checkbox"]', spellFilter).click(function(event) {
+      spellFilter.submit();
+    });
+
+    return spellFilter;
+  };
+
+  $.fn.ajaxSearch = function() {
+    var searchInput = this;
+
+    $('input#search', searchInput).keyup(function(event) {
+      if (this.value.length >= 3 || this.value.length == 0) {
+        $('input#search', searchInput).parent().submit();
+      }
+    });
+  }
+}(jQuery));
